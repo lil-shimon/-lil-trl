80a44c437e1e6b010750d289ba505120533d010d
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-07-18 17:54:34 +0900

    test: 💍 added test

--- src/features/useEffect/apiUseEffect.test.tsx
+++ src/features/useEffect/apiUseEffect.test.tsx
@@ -3,8 +3,13 @@ import { render, screen } from "@testing-library/react";
 import { ApiUseEffectComponent } from "./apiUseEffect";
 
 describe("useEffect rendering", () => {
+    it("should render null when rendering", () => {
+        const {container} = render(<ApiUseEffectComponent/>)
+        expect(container.firstChild).toBeNull()
+    })
+
     it('should render only after async func resolved', async () => {
-        render(<ApiUseEffectComponent />)
+        render(<ApiUseEffectComponent/>)
         expect(screen.queryByText(/I am/)).toBeNull();
         expect(await screen.findByText(/I am/)).toBeInTheDocument();
     })
